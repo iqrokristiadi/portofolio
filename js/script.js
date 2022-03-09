95c93261dc843517bb9c1fd96393acd5ff6cb735
iqrokristiadi <iqro.kristiadi2004@gmail.com>
2022-03-09 21:48:37 +0700

    jumbotron parallax

--- js/script.js
+++ js/script.js
@@ -13,7 +13,22 @@ $('.page-scroll').on('click' , function(e){
 	
 	e.preventDefault();
 
-})
+});
 
 // Parallax
 
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+});
